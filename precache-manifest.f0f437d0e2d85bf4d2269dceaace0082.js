9d0963f8d675680a5c70e65081aab5a2561a18d5
dragonfist453 <thermosphere453@gmail.com>
2022-06-25 13:50:10 +0530

    Updates

---
+++ precache-manifest.f0f437d0e2d85bf4d2269dceaace0082.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddd22324af2b91dcae6b",
+    "url": "/light-state-client/static/css/main.4e83aeb6.chunk.css"
+  },
+  {
+    "revision": "ddd22324af2b91dcae6b",
+    "url": "/light-state-client/static/js/main.ddd22324.chunk.js"
+  },
+  {
+    "revision": "d24481045266d2cf7742",
+    "url": "/light-state-client/static/js/1.d2448104.chunk.js"
+  },
+  {
+    "revision": "ad80ef48b2988d4c5430",
+    "url": "/light-state-client/static/js/runtime~main.ad80ef48.js"
+  },
+  {
+    "revision": "9927e20f4acea6e8dadd9a89890dcd35",
+    "url": "/light-state-client/index.html"
+  }
+];
